3f2c86bada8ecfd953459760f2893b379e96f84f
aman07-ctrl <75978632+aman07-ctrl@users.noreply.github.com>
2026-05-16 17:42:22 +0530

    Bulk sync: 1 submission

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean hasDuplicate(int[] nums) {
+        Arrays.sort(nums);
+        boolean flag=false;
+        for(int i=0;i<nums.length-1;i++){
+            if(nums[i]==nums[i+1]){
+                flag= true;
+            }
+        }
+        return flag;
+    }
+}
